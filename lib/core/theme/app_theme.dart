189ab19db585469c91a3042fea8dfd4c89dd5d9a
LabCoop Dev <dev@labcoop.app>
2026-07-01 08:04:59 +0800

    Fix: rename CardTheme to CardThemeData for latest Flutter API

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -27,7 +27,7 @@ class AppTheme {
         elevation: 0,
         centerTitle: true,
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 2,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
